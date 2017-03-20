770594b20df5f3a87064845c0be75107dcc08439
Jonatan Östling <jonatan.ostling@gmail.com>
2017-03-20 16:03:22 +0100

    Fixed JS-bug

--- js/main.js
+++ js/main.js
@@ -3,8 +3,6 @@ $(document).ready(function() {
 // Get the modal
 var modal1 = document.getElementById('myModal1');
 var modal2 = document.getElementById('myModal2');
-var modal3 = document.getElementById('myModal3');
-var modal4 = document.getElementById('myModal4');
 
 // Get the image and insert it inside the modal - use its "alt" text as a caption
 var img1 = document.getElementById('myImg1');
@@ -24,24 +22,6 @@ img2.onclick = function(){
     modalImg2.src = this.src;
     captionText2.innerHTML = this.alt;
 }
-
-var img3 = document.getElementById('myImg3');
-var modalImg3 = document.getElementById("img03");
-var captionText3 = document.getElementById("caption3");
-img3.onclick = function(){
-    modal3.style.display = "block";
-    modalImg3.src = this.src;
-    captionText3.innerHTML = this.alt;
-}
-
-var img4 = document.getElementById('myImg4');
-var modalImg4 = document.getElementById("img04");
-var captionText4 = document.getElementById("caption4");
-img4.onclick = function(){
-    modal4.style.display = "block";
-    modalImg4.src = this.src;
-    captionText4.innerHTML = this.alt;
-}
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
@@ -54,11 +34,5 @@ modal1.onclick = function() {
 }
 modal2.onclick = function() {
   modal2.style.display = "none";
-}
-modal3.onclick = function() {
-  modal3.style.display = "none";
-}
-modal4.onclick = function() {
-  modal4.style.display = "none";
 }
 });

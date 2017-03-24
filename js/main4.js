5c662f4b551066f167dee873a9fe6af451e1ba7c
Jonatan Östling <jonatan.ostling@gmail.com>
2017-03-24 13:18:34 +0100

    new images and a carousel on first page

---
+++ js/main4.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  $('.carousel').carousel({
+    interval: 6500,
+    pause: null
+  });
+  $.each( jQuery('.carousel .item'), function( i, val ) {
+    $(this).css('background-image','url('+$(this).find('img').attr('src')+')').css('background-size','cover').find('img').css('visibility','hidden');
+  });
+  if ($('.mobile-indicator').css('display') == 'none') {
+    $(".carousel-caption").mouseenter(function(){
+      clearTimeout($(this).data('timeoutId'));
+      $(this).find(".carousel-button").slideDown(300);
+
+    }).mouseleave(function(){
+      var button = $(this),
+      timeoutId = setTimeout(function(){
+        button.find(".carousel-button").slideUp(300);
+      }, 200);
+      //set the timeoutId, allowing us to clear this trigger if the mouse comes back over
+      button.data('timeoutId', timeoutId);
+    });
+  }
+});

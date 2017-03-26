8ff27310fc666ce56c29bd0a7ce32663fe2c8934
Jonatan Östling <jonatan.ostling@gmail.com>
2017-03-26 14:17:12 +0200

    Changed mobile view for optimal looks

--- js/main4.js
+++ js/main4.js
@@ -4,20 +4,20 @@ $(document).ready(function() {
     pause: null
   });
   $.each( jQuery('.carousel .item'), function( i, val ) {
-    $(this).css('background-image','url('+$(this).find('img').attr('src')+')').css('background-size','cover').find('img').css('visibility','hidden');
+    $(this).css('background-image','url('+$(this).find('img').attr('src')+')').css('background-size', 'cover').find('img').css('visibility','hidden');
   });
-  if ($('.mobile-indicator').css('display') == 'none') {
-    $(".carousel-caption").mouseenter(function(){
-      clearTimeout($(this).data('timeoutId'));
-      $(this).find(".carousel-button").slideDown(300);
-
-    }).mouseleave(function(){
-      var button = $(this),
-      timeoutId = setTimeout(function(){
-        button.find(".carousel-button").slideUp(300);
-      }, 200);
-      //set the timeoutId, allowing us to clear this trigger if the mouse comes back over
-      button.data('timeoutId', timeoutId);
-    });
-  }
+  // // if ($('.mobile-indicator').css('display') == 'none') {
+  // //   $(".carousel-caption").mouseenter(function(){
+  // //     clearTimeout($(this).data('timeoutId'));
+  // //     $(this).find(".carousel-button").slideDown(300);
+  // //
+  // //   }).mouseleave(function(){
+  // //     var button = $(this),
+  // //     timeoutId = setTimeout(function(){
+  // //       button.find(".carousel-button").slideUp(300);
+  // //     }, 200);
+  // //     //set the timeoutId, allowing us to clear this trigger if the mouse comes back over
+  // //     button.data('timeoutId', timeoutId);
+  // //   });
+  // }
 });

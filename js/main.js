f08acbaf36314e2dc63f26ea6fc24dbd1272843b
Jonatan Östling <jonatan.ostling@gmail.com>
2017-03-27 02:18:27 +0200

    Resized images, css, js, html files

--- js/main.js
+++ js/main.js
@@ -1,10 +1,6 @@
 $(document).ready(function() {
-
-// Get the modal
 var modal1 = document.getElementById('myModal1');
 var modal2 = document.getElementById('myModal2');
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
 var img1 = document.getElementById('myImg1');
 var modalImg1 = document.getElementById("img01");
 var captionText1 = document.getElementById("caption1");
@@ -13,7 +9,6 @@ img1.onclick = function(){
     modalImg1.src = this.src;
     captionText1.innerHTML = this.alt;
 }
-
 var img2 = document.getElementById('myImg2');
 var modalImg2 = document.getElementById("img02");
 var captionText2 = document.getElementById("caption2");
@@ -22,10 +17,7 @@ img2.onclick = function(){
     modalImg2.src = this.src;
     captionText2.innerHTML = this.alt;
 }
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
--- js/main3.js
+++ js/main3.js
@@ -1,9 +1,5 @@
 $(document).ready(function() {
-
-// Get the modal
 var modal4 = document.getElementById('myModal4');
-
-// Get the image and insert it inside the modal - use its "alt" text as a caption
 var img4 = document.getElementById('myImg4');
 var modalImg4 = document.getElementById("img04");
 var captionText4 = document.getElementById("caption4");
@@ -12,11 +8,7 @@ img4.onclick = function(){
     modalImg4.src = this.src;
     captionText4.innerHTML = this.alt;
 }
-
-// Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
